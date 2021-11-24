4f3d2c613cab494b82cd27d2810dbe048ac595e1
Oleksiy Blavat <oleksiy.blavat@amobee.com>
2021-11-23 20:43:05 -0500

    initial commit

---
+++ src/main/java/com/randomproblems/RandomProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.randomproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomProblemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/randomproblems/RandomProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
